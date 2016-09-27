91198bf4a6638e40a02b9651a8ac69e80aad4bf9
Xavid Pretzer <xavid@mit.edu>
2016-09-27 17:23:19 -0400

    Separate out cover, booklet, and back config fields.

--- bin/book_lib.py
+++ bin/book_lib.py
@@ -1,3 +1,5 @@
+import yaml
+
 from prod_lib import *
 
 def get_book_paths(run=None, use_names=False, pdfdir=None, macros=None):
@@ -14,6 +16,21 @@ def get_book_paths(run=None, use_names=False, pdfdir=None, macros=None):
             get_text(GAMEKI_DIR + 'lib/charnames.tex',
                      run=run).strip().split('\n')]
 
+    cover = 'Gameki/Templates/cover.tex'
+    booklet = ['listchar', 'listblue', 'listgreen']
+    back = 'abilstats'
+    try:
+        config = yaml.safe_load(open(GAMEKI_DIR + 'config.yaml'))
+    except IOError:
+        pass
+    else:
+        if 'cover' in configL
+            cover = config['cover']
+        if 'booklet' in config:
+            booklet = config['booklet']
+        if 'back' in config:
+            back = config['back']
+
     run_infix = ''
     if not pdfdir:
         pdfdir = 'Out/book/'
@@ -23,14 +40,20 @@ def get_book_paths(run=None, use_names=False, pdfdir=None, macros=None):
     rules = get_pdf_path('Handouts/rules-scenario.tex')
     for i in xrange(len(macros)):
         m = macros[i]
-        cover = get_pdf_path('Gameki/Templates/cover.tex', m, run=run)
-        image = get_pdf_path('image-%s%s' % (run_infix, m))
-        body = get_pdf_path('listchar-%s%s' % (run_infix, m), color_sheets=True)
-        blues = get_pdf_path('listblue-%s%s' % (run_infix, m),
-                             color_sheets=True)
-        #body = get_pdf_path('Charsheets/%s.tex' % m[2:], run=run)
-        #statcard = get_pdf_path('statcards-%s%s' % (run_infix, m))
-        statcard = get_pdf_path('abilstats-%s%s' % (run_infix, m))
+
+        def get_page_path(p):
+            if '/' in p:
+                return get_pdf_path(p, m, run=run)
+            else:
+                return get_pdf_path('%s-%s%s' % (p, run_infix, m),
+                                    color_sheets=True)
+
+        sheets = [cover, rules]
+        for p in booklet:
+            sheet = get_page_path(p)
+            if sheet:
+                sheets.append(sheet)
+        back_page = get_page_path(back)
 
         try:
             os.makedirs(pdfdir)
@@ -41,14 +64,11 @@ def get_book_paths(run=None, use_names=False, pdfdir=None, macros=None):
         else:
             name = m[1:]
         joined = '%s/%s%s-packet.pdf' % (pdfdir, run_infix, name)
-        sheets = [cover, rules, image, body]
-        if blues:
-            sheets.append(blues)
         run_cmd('pdfjoin', ['-o', joined] + sheets)
         yield joined
         statcard_out = '%s/%s%s-statcard.pdf' % (pdfdir, run_infix, name)
-        run_cmd('cp', [statcard, statcard_out])
+        run_cmd('cp', [back_page, statcard_out])
         yield statcard_out
         book = '%s/%s%s-booklet.pdf' % (pdfdir, run_infix, name)
-        run_cmd(GAMEKI_DIR + 'bin/backbook.sh', [joined, statcard, book])
+        run_cmd(GAMEKI_DIR + 'bin/backbook.sh', [joined, back_page, book])
         yield book
